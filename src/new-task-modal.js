bb05f66b1bab588a58a8180db4ce66338b8895d4
emuel-vassallo <emuelvassallo15@gmail.com>
2022-07-03 18:11:40 +0200

    add unfunctional priority dropdown

--- src/new-task-modal.js
+++ src/new-task-modal.js
@@ -7,33 +7,62 @@ const NewTaskModal = (() => {
   );
   const modalCancelButton = document.querySelector('.cancel-button');
   const dueDatePicker = document.querySelector('.due-date-picker');
+  const priorityButton = document.querySelector('.priority-selector');
+  const priorityDropdownMenu = document.querySelector(
+    '.priority-dropdown-menu'
+  );
   const submitButton = document.querySelector('.add-task-submit-button');
   const newTaskModalOverlay = document.querySelector(
     '.add-task-modal-overlay '
   );
 
+  // Modal
   const toggleNewTaskModal = () => newTaskModal.classList.toggle('visible');
-  const toggleOverlay = () => newTaskModalOverlay.classList.toggle('visible');
+  const toggleModalOverlay = () =>
+    newTaskModalOverlay.classList.toggle('visible');
   const clearModal = () => addTaskForm.reset();
+  const isModalVisible = () => newTaskModal.classList.contains('visible');
+
+  // Task Name
+  const focusTaskNameInput = () => taskNameInput.focus();
+
+  // Priority
+  const togglePriorityDropdown = () => {
+    priorityDropdownMenu.classList.toggle('visible');
+    priorityButton.classList.toggle('selected');
+  };
+  const hidePriorityDropDown = () => {
+    priorityDropdownMenu.classList.remove('visible');
+    priorityButton.classList.remove('selected');
+  };
+  // Due Date
   const changeDefaultDueDate = () => (dueDatePicker.valueAsDate = new Date());
+
+  // Submit Button
   const enableSubmitButton = () => (submitButton.disabled = false);
   const disableSubmitButton = () => (submitButton.disabled = true);
-  const focusTaskNameInput = () => taskNameInput.focus();
-  const isModalVisible = () => newTaskModal.classList.contains('visible');
+
   const toggleModal = () => {
     toggleNewTaskModal();
-    toggleOverlay();
+    toggleModalOverlay();
     clearModal();
     changeDefaultDueDate();
     focusTaskNameInput();
     disableSubmitButton();
+    hidePriorityDropDown();
   };
 
+  // Event Listeners
   addTaskButton.addEventListener('click', () => toggleModal());
+
   modalCancelButton.addEventListener('click', () => toggleModal());
+
   window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && isModalVisible()) toggleModal();
   });
+
+  priorityButton.addEventListener('click', () => togglePriorityDropdown());
+
   newTaskModalOverlay.addEventListener('click', (e) => {
     const clickedElementParent = e.target.offsetParent;
     const isModalClicked =
